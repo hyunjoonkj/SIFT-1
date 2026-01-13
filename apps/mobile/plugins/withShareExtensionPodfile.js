9d0bc1cfb34a46c328e49cf5737912f95ea14bf8
junsaegae <rykjun@gmail.com>
2026-01-12 21:06:28 -0500

    fix(ios): inject config_command to fix ShareExtension autolinking

--- apps/mobile/plugins/withShareExtensionPodfile.js
+++ apps/mobile/plugins/withShareExtensionPodfile.js
@@ -5,7 +5,24 @@ const withShareExtensionPodfile = (config) => {
         const targetBlock = `
 target 'ShareExtension' do
   use_expo_modules!
-  config = use_native_modules!
+
+  if ENV['EXPO_USE_COMMUNITY_AUTOLINKING'] == '1'
+    config_command = ['node', '-e', "process.argv=['', '', 'config'];require('@react-native-community/cli').run()"];
+  else
+    config_command = [
+      'node',
+      '--no-warnings',
+      '--eval',
+      'require(\\'expo/bin/autolinking\\')',
+      'expo-modules-autolinking',
+      'react-native-config',
+      '--json',
+      '--platform',
+      'ios'
+    ]
+  end
+
+  config = use_native_modules!(config_command)
   
   # Inherit search paths and configuration from the main project
   inherit! :complete
